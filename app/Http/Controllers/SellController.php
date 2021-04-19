ed02b15eaa12b4d0a09c4bf87142aa97e8fe5088
Soph3ak <sounsopheak7@gmail.com>
2021-04-20 06:49:12 +0700

    add orders

--- app/Http/Controllers/SellController.php
+++ app/Http/Controllers/SellController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Collection;
 
 class SellController extends Controller
 {
@@ -12,8 +13,14 @@ class SellController extends Controller
         return $categories = Category::all();
     }
 
-
     public function loadProductsByCategory($id){
-        return $products = Category::find($id)->products;
+        $products = Category::find($id)->products;
+        $cols = collect($products);
+        /*$cols = $cols->each(function ($item, $key) {
+            $item->id+=1;
+        });*/
+        return $cols;
+
+
     }
 }
--- resources/js/components/Sell.vue
+++ resources/js/components/Sell.vue
@@ -10,7 +10,7 @@
                                     <h2>Categories</h2>
                                 </div>
                                 <!-- Gallery item -->
-                                <div v-for="category in categories" :key="category.id" @click="loadProductsByCategory(category.id)" class="col-xl-4 col-lg-4 col-md-6 mb-lg-4 mb-md-3">
+                                <div v-for="category in categories" :key="category.id" @click="loadProductsByCategory(category.id)" class="col-xl-4 col-lg-4 col-md-6 mb-lg-4 mb-md-3" id="sell-category">
                                     <div class="bg-success rounded shadow-sm sell-card d-flex flex-column">
                                         <img :src="'/icons/'+category.name+'.jpg'" class="rounded">
                                         <div class="cate-text p-lg-4 p-md-2">
@@ -29,9 +29,9 @@
                                     <a href="#" class="mr-4">Tables</a>
                                     <a href="#" class="mr-4">Customers</a>
                                 </div>
-                                <div v-for="(product,index ) in products" :key="product.id" @click="increase(index)" class="col-xl-4 col-lg-4 col-md-6 mb-lg-4 mb-md-3">
+                                <div v-for="(product,index ) in products" :key="product.id" @click="increase(index, product.id)" class="col-xl-4 col-lg-4 col-md-6 mb-lg-4 mb-md-3" id="sell-product">
                                     <div class="rounded shadow-sm sell-card">
-                                        <img src="/files/1617893850.99823.តុងយាំបង្កង.jpg" alt="Product Image" class="img-size-50 rounded">
+                                        <img :src="'/files/'+product.photo" alt="Product Image" class="rounded" style="width: 100%; height: 100%">
                                         <div class="cate-text position-absolute d-flex align-items-start flex-column">
                                             <h1 class="p-lg-4 p-md-3 text-gray mb-auto qty">{{ qtys[index].qty }}</h1>
                                             <h5 class="p-lg-3 pl-lg-4 p-md-2 pl-md-3">{{product.price}}៛</h5>
@@ -42,53 +42,7 @@
                         </div>
                     </div>
                 </div>
-                <!--<div class="px-3 product">
-                    <div class="row">
-                        <div class="header pl-3">
-                            <h2>FUCK</h2>
-                        </div>
-                        &lt;!&ndash; Gallery item &ndash;&gt;
-                        <div class="col-4 mb-2">
-                            <div class="bg-white rounded shadow-sm sell-card">
-                                &lt;!&ndash;<img src="https://res.cloudinary.com/mhmd/image/upload/v1556294929/matthew-hamilton-351641-unsplash_zmvozs.jpg" alt="" class="img-fluid card-img-top">&ndash;&gt;
-                            </div>
-                        </div>
-                        <div class="col-4 mb-4">
-                            <div class="bg-white rounded shadow-sm sell-card">
-                            </div>
-                        </div>
-                        <div class="col-4 mb-4">
-                            <div class="bg-white rounded shadow-sm sell-card">
-                            </div>
-                        </div>
-                        <div class=" col-4 mb-4">
-                            <div class="bg-white rounded shadow-sm sell-card">
-                            </div>
-                        </div>
-                        <div class="col-4 mb-4">
-                            <div class="bg-white rounded shadow-sm sell-card">
-                            </div>
-                        </div>
-                        <div class="col-4 mb-4">
-                            <div class="bg-white rounded shadow-sm sell-card">
-                            </div>
-                        </div>
-                        <div class="col-4 mb-4">
-                            <div class="bg-white rounded shadow-sm sell-card">
-                            </div>
-                        </div>
-                        <div class="col-4 mb-2">
-                            <div class="bg-white rounded shadow-sm sell-card">
-                            </div>
-                        </div>
-                        <div class="col-4 mb-4">
-                            <div class="bg-white rounded shadow-sm sell-card">
-                            </div>
-                        </div>
-                        &lt;!&ndash; End &ndash;&gt;
 
-                    </div>
-                </div>-->
             </div>
             <div class="col-lg-3 col-md-5 col-sm-5 banner pr-lg-5 d-flex flex-column">
                 <div class="header">
@@ -98,82 +52,27 @@
                     <div class="card-body table-responsive p-0">
                         <table class="table table-head-fixed text-nowrap">
                             <tbody>
-                            <tr>
+                            <tr v-for="(order, index) in orders" :key="index">
                                 <td class="products-list" style="padding-left: 12px">
                                     <div class="product-img">
-                                        <img src="/files/1617893850.99823.តុងយាំបង្កង.jpg" alt="Product Image" class="img-size-50 rounded">
+                                        <img :src="'/files/'+order.image" alt="Product Image" class="img-size-50 rounded">
+
                                     </div>
                                     <div class="product-info">
-                                        <a href="javascript:void(0)" class="product-title">តុំយុាំគ្រឿងបង្កង</a>
-                                        <span data-v-7e94e6d4="" class="product-description">តុំយុាំគ្រឿងបង្កង</span>
+                                        <a href="javascript:void(0)" class="product-title">{{ order.name_kh }}</a>
+                                        <span class="product-description">{{ order.name }}</span>
                                     </div>
                                 </td>
                                 <td class="px-md-0">
                                     <a type="button" class="testbutton"><i class="ion-minus"></i></a>
-                                    <span class="m-lg-2 m-md-1">2</span>
-                                    <a type="button" class="testbutton"><i class="ion-plus"></i></a>
+                                    <span class="m-lg-2 m-md-1">{{ order.qty }}</span>
+                                    <a type="button" @click="increase(order.index, order.id)" class="testbutton"><i class="ion-plus"></i></a>
                                 </td>
                                 <td class="text-right">
-                                    20,000៛
+                                    {{ order.amount }}៛
                                 </td>
                             </tr>
-                            <tr>
-                                <td class="products-list" style="padding-left: 12px">
-                                    <div class="product-img">
-                                        <img src="/files/1617893850.99823.តុងយាំបង្កង.jpg" alt="Product Image" class="img-size-50 rounded">
-                                    </div>
-                                    <div class="product-info">
-                                        <a href="javascript:void(0)" class="product-title">តុំយុាំគ្រឿងបង្កង</a>
-                                        <span data-v-7e94e6d4="" class="product-description">តុំយុាំគ្រឿងបង្កង</span>
-                                    </div>
-                                </td>
-                                <td class="px-md-0">
-                                    <a type="button" class="testbutton"><i class="ion-minus"></i></a>
-                                    <span class="m-lg-2 m-md-1">2</span>
-                                    <a type="button" class="testbutton"><i class="ion-plus"></i></a>
-                                </td>
-                                <td class="text-right">
-                                    20,000៛
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="products-list" style="padding-left: 12px">
-                                    <div class="product-img">
-                                        <img src="/files/1617893850.99823.តុងយាំបង្កង.jpg" alt="Product Image" class="img-size-50 rounded">
-                                    </div>
-                                    <div class="product-info">
-                                        <a href="javascript:void(0)" class="product-title">តុំយុាំគ្រឿងបង្កង</a>
-                                        <span data-v-7e94e6d4="" class="product-description">តុំយុាំគ្រឿងបង្កង</span>
-                                    </div>
-                                </td>
-                                <td class="px-md-0">
-                                    <a type="button" class="testbutton"><i class="ion-minus"></i></a>
-                                    <span class="m-lg-2 m-md-1">2</span>
-                                    <a type="button" class="testbutton"><i class="ion-plus"></i></a>
-                                </td>
-                                <td class="text-right">
-                                    20,000៛
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="products-list" style="padding-left: 12px">
-                                    <div class="product-img">
-                                        <img src="/files/1617893850.99823.តុងយាំបង្កង.jpg" alt="Product Image" class="img-size-50 rounded">
-                                    </div>
-                                    <div class="product-info">
-                                        <a href="javascript:void(0)" class="product-title">តុំយុាំគ្រឿងបង្កង</a>
-                                        <span data-v-7e94e6d4="" class="product-description">តុំយុាំគ្រឿងបង្កង</span>
-                                    </div>
-                                </td>
-                                <td class="px-md-0">
-                                    <a type="button" class="testbutton"><i class="ion-minus"></i></a>
-                                    <span class="m-lg-2 m-md-1">2</span>
-                                    <a type="button" class="testbutton"><i class="ion-plus"></i></a>
-                                </td>
-                                <td class="text-right">
-                                    20,000៛
-                                </td>
-                            </tr>
+
                             </tbody>
                         </table>
                     </div>
@@ -182,15 +81,15 @@
                 <div class="pay mt-auto d-flex flex-column">
                     <div class="subtotal d-flex">
                         <h5>Subtotal</h5>
-                        <h4 class="ml-auto">80,000៛</h4>
+                        <h4 class="ml-auto">{{ subTotal }}.00៛</h4>
                     </div>
                     <div class="discount d-flex">
                         <h5>Discount</h5>
-                        <h4 class="ml-auto">0.00៛</h4>
+                        <h4 class="ml-auto">{{ discount }}.00៛</h4>
                     </div>
                     <div class="total d-flex">
                         <h5>Total</h5>
-                        <h4 class="ml-auto">80,000៛</h4>
+                        <h4 class="ml-auto">{{total}}.00៛</h4>
                     </div>
                     <button class="btn btn-block btn-success mb-3 mt-2">Pay</button>
                     <div class="small-icon d-flex justify-content-between mt-auto">
@@ -207,13 +106,26 @@
 </template>
 
 <script>
+    import {getValue} from "vue-currency-input";
+
     export default {
+        /*props: {
+            colours: {
+                type: Array,
+                required: true,
+            },
+        },*/
         data () {
             return{
                 categories:{},
-                products:{},
+                products:[],
                 qtys: [],
                 tmp:0,
+                orders: [],
+
+                subTotal:0,
+                discount:0,
+                total:0,
             }
         },
         methods:{
@@ -224,31 +136,93 @@
                     });
             },
 
-            loadProductsByCategory(cate){
-                console.log('category_id: '+cate)
-                if(cate !== this.tmp){
-                    axios.get('api/loadProductsByCategory/'+cate)
+            loadProductsByCategory(cateID){
+                console.log('category_id: '+cateID)
+                if(cateID !== this.tmp){
+                    axios.get('api/loadProductsByCategory/'+cateID)
                         .then(response => {
                             this.products = response.data;
-
                             this.qtys = []
                             for (let pro in this.products){
                                 this.qtys.push({
-                                    qty: 0,
+                                    'qty': 0,
                                 });
                             }
-                        });
-                    this.tmp = cate
+                        })
+                        .catch(error => console.log(error))
+                        .finally(() => this.loading = false)
+                    this.tmp = cateID
                 }
+
             },
 
-            increase(i){
-                this.qtys[i].qty++
-                let qty = this.qtys[i].qty
-                let selector = $("h1.qty:eq("+i+")");
+            increase(index, proID){
+                this.qtys[index].qty++
+                let qty = this.qtys[index].qty
+                let selector = $("h1.qty:eq("+index+")");
                 if (qty > 0){
                     selector.removeClass('text-gray');
                 }
+                this.addOrder(index,proID)
+            },
+
+            addOrder (i, proID) {
+                if(this.orders.length > 0){
+                    for(let j=0; j<this.orders.length; j++){
+                        if(proID === this.orders[j].id){
+                            let qty = (this.orders[j].qty += 1);
+                            this.orders[j].amount = this.orders[j].price * qty
+                            return 0
+                        }
+                    }
+
+                    this.orders.push({
+                        'id':this.products[i].id,
+                        'index': i,
+                        'name_kh':this.products[i].name_kh,
+                        'name':this.products[i].name,
+                        'price': this.products[i].price,
+                        'qty':1,
+                        'amount': this.products[i].price,
+                        'image':this.products[i].photo,
+                    })
+                }
+                else {
+                    this.orders.push({
+                        'id':this.products[i].id,
+                        'index': i,
+                        'name_kh':this.products[i].name_kh,
+                        'name':this.products[i].name,
+                        'price': this.products[i].price,
+                        'qty':1,
+                        'amount': this.products[i].price,
+                        'image':this.products[i].photo,
+                    });
+                }
+
+            },
+
+            calSubTotal(){
+                let total = [];
+                Object.entries(this.orders).forEach(([key, val]) => {
+                    total.push(val.amount) // the value of the current key.
+                });
+                return this.subTotal = total.reduce(function(total, num){ return total + num }, 0);
+            },
+
+            myTest(){
+
+            },
+
+        },
+        watch: {
+            subTotal(){
+                return this.total =  this.subTotal - this.discount
+            },
+
+            orders: {
+                handler: function (val, oldVal) { this.calSubTotal() },
+                deep: true
             },
 
         },
@@ -256,22 +230,6 @@
         mounted() {
             console.log('mounted')
            this.loadCategoriesSell()
-
-            /*let card_text = $(".card-text");
-            card_text.hover(function (){
-                $(this).hide()
-                let img = $(".card-text ~ img");
-                img.hide()
-            })*/
-
-            $(document).ready(function(){
-                $("h1").hover(function(){
-
-                    $(this).css("background-color", "yellow");
-                }, function(){
-                    $(this).css("background-color", "pink");
-                });
-            });
 
         }
     }
